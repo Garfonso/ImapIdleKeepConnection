1d975781c52fb3226a542e2a59946bd78a7f637b
Garfonso <garfonso@mobo.info>
2023-03-23 09:27:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ImapIdleKeepConnection
+ use node-imap to connect to an imap server and handle all errors. Only emit mail event
---
+++ index.js
@@ -0,0 +1,168 @@
+/*jslint node: true, es6: true */
+/*jshint esversion: 6*/
+
+/**
+ * Created by garfo on 20.10.2016.
+ */
+
+const Imap = require('node-imap'); //use our own imap. But we keep npm imap installed to have the dependencies installed, too. Check if this works.
+
+class ImapIdleConnectionAndEvent extends EventEmitter {
+    name = '';
+    connecting = true;
+    active = false;
+    intervalHandler;
+    disconnects = 0;
+    numMailRuns = 0;
+    retriesSinceLastSuccess = 0;
+    timeoutHandler;
+    mailbox = 'INBOX';
+
+    log = {
+        error: () => { console.log(arguments); },
+        debug: () => { console.log(arguments); },
+        info: () => { console.log(arguments); }
+    }
+
+    imap;
+
+    reconnect(funcName) {
+        if (this.timeoutHandler) {
+            clearTimeout(this.timeoutHandler);
+            this.timeoutHandler = false;
+        }
+        this.timeoutHandler = setTimeout(() => {
+            if (this.imap.state !== 'connected' && this.imap.state !== 'authenticated' && !this.connecting) {
+                if (this.retriesSinceLastSuccess < 500) {
+                    this.log.error('Reconnecting -', funcName);
+                    this.log.error('Imap state:', this.imap.state, 'active flag:', this.active, 'connecting flag:', this.connecting);
+                    this.connecting = true;
+                    this.imap.connect();
+                    this.disconnects += 1;
+                    this.retriesSinceLastSuccess += 1;
+                } else {
+                    this.error('Already tried to reconnect', this.retriesSinceLastSuccess, 'times. Will stop now to avoid bans. Please check credentials.');
+                }
+            } else if (this.connecting) {
+                this.log.debug('Already connecting -', funcName);
+                this.log.debug('Imap state:', this.imap.state, 'active flag:', this.active, 'connecting flag:', this.connecting);
+            }
+        }, Math.round(Math.random() * 30000) + 120000); //between 120s and 150s.
+    }
+
+    onOpenBox(err /*, box */) {
+        this.log.debug('onOpenBox');
+        if (err) {
+            this.log.error('Error in opening box:', err);
+        } else {
+            //params.debug("Box:", box);
+            this.log.log('Idle at work now.');
+        }
+    }
+
+    onReady() {
+        this.log.info('Successful connected & authenticated.');
+        //open read only.
+        this.retriesSinceLastSuccess = 0;
+        this.imap.openBox(this.mailbox, true, () => this.onOpenBox());
+        this.connecting = false;
+        this.active = true;
+    }
+
+    onMail(numNewMsgs, noSender = false) {
+        this.log.info('new Mail');
+        this.numMailRuns += 1;
+        this.emit('mail', numNewMsgs);
+    }
+
+    onError(err) {
+        this.log.info('Have error: ', err);
+        this.active = false;
+        this.connecting = false;
+        this.reconnect('Error');
+    }
+
+    onClose(err) {
+        //Can this happen without error before?
+        this.log.info('Have close: ', err);
+        this.active = false;
+        this.connecting = false;
+        this.reconnect('Close');
+    }
+
+    onEnd() {
+        this.log.info('Connection ended...');
+        this.active = false;
+        this.connecting = false;
+        this.reconnect('End');
+    }
+
+
+    /**
+     *
+     * @param params: {
+     *     name: name of the idle watcher, needs to be unique!
+     *     user
+     *     password
+     *     host
+     *     port
+     *     tls
+     *     mailbox
+     *
+     *     log
+     *     debug
+     *     error
+     *   }
+     */
+    constructor(params) {
+        super();
+
+        this.imap = new Imap({
+            user: params.user,
+            password: params.password,
+            host: params.host,
+            port: params.port,
+            tls: params.tls,
+
+            //idle stuff:
+            keepalive: {
+                interval: 10000, //default 10000ms - NOOPs are send & idleInterval is checked
+                idleInterval: 120000, //default 300.000ms - interval at which IDLE command is resend - set to 2min on 12.12.2016
+                // -> ca. 150.000ms seems to be necessary for outlook. So have this at 120s.
+                // -> now very few disconnects.. let's see.
+                // -> profihost sometimes "connection end" -> i.e. no error. Hm. Too many?
+                forceNoop: false //default: false - force use of NOOP keepalice on servers also support IDLE
+            }
+        });
+
+        this.imap.on('ready', () => this.onReady());
+        this.imap.on('mail', (numNewMessages, noSender) => this.onMail(numNewMessages, noSender));
+        this.imap.on('error', () => this.onError());
+        this.imap.on('close', () => this.onClose());
+        this.imap.on('end', () => this.onEnd());
+        this.imap.on('ready', () => this.onReady());
+
+        if (typeof params.log === 'function') {
+            this.log.info = params.log;
+        }
+        if (typeof params.debug === 'function') {
+            this.log.debug = params.debug;
+        }
+        if (typeof params.error === 'function') {
+            this.log.error = params.error;
+        }
+
+        this.imap.connect();
+    }
+
+    endWatch() {
+        clearTimeout(this.timeoutHandler);
+        this.imap.end();
+    }
+
+    getStatus() {
+        return this.imap.state + '\t#mailruns:\t' + this.numMailRuns + '\t#disconnects:\t' + this.disconnects + '\t#retriesSinceLastSuccess:\t' + this.retriesSinceLastSuccess + '\n';
+    }
+}
+
+module.exports = ImapIdleConnectionAndEvent;
